cdb82b7bfb1592afebee0ff9e9b5928f4dcd0f95
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-04-29 10:32:22 +0530

    add config part

---
+++ src/main/java/org/viva/icet/config/ConfigModelMapper.java
@@ -0,0 +1,14 @@
+package org.viva.icet.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigModelMapper {
+    @Bean
+    public ModelMapper getMapper() {
+        return new ModelMapper();
+    }
+
+}
